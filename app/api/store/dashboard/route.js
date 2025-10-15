fb9c24ad60d03be04bf879b6d5707bda71b70ff1
kiettt23 <kiettt23@gmail.com>
2025-10-16 00:13:38 +0700

    fix(api-store-dashboard): correct field name from totalAmouth to total in earnings calculation

--- app/api/store/dashboard/route.js
+++ app/api/store/dashboard/route.js
@@ -24,7 +24,7 @@ export async function GET(request) {
       ratings,
       totalOrders: orders.length,
       totalEarnings: Math.round(
-        orders.reduce((acc, order) => acc + order.totalAmount, 0)
+        orders.reduce((acc, order) => acc + order.total, 0)
       ),
       totalProducts: products.length,
     };
