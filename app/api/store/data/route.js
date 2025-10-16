e2dbdff0a5a08919d0e740cda66d2b26a1a9a615
kiettt23 <kiettt23@gmail.com>
2025-10-16 14:43:46 +0700

    fix(api): correct product relation name in store data route

--- app/api/store/data/route.js
+++ app/api/store/data/route.js
@@ -16,7 +16,7 @@ export async function GET(request) {
     const store = await prisma.store.findUnique({
       where: { username, isActive: true },
       include: {
-        Products: {
+        Product: {
           include: {
             rating: true,
           },
