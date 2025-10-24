e339a0be04e00fe6c26df590333a207e0118f91d
kiettt23 <kiettt23@gmail.com>
2025-10-24 15:25:57 +0700

    feat(payments): add Stripe Checkout flow and harden webhook handling

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,7 +1,9 @@
+import { metadata } from "@/app/layout";
 import prisma from "@/lib/prisma";
 import { getAuth } from "@clerk/nextjs/server";
 import { PaymentMethod } from "@prisma/client";
 import { NextResponse } from "next/server";
+import Stripe from "stripe";
 
 export async function POST(request) {
   try {
@@ -120,6 +122,37 @@ export async function POST(request) {
         },
       });
       orderIds.push(order.id);
+    }
+
+    if (paymentMethod === "STRIPE") {
+      const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+      const origin = await request.headers.get("origin");
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price_data: {
+              currency: "vnd",
+              product_data: {
+                name: "Order",
+              },
+              unit_amount: Math.round(fullAmount * 100),
+            },
+            quantity: 1,
+          },
+        ],
+        expires_at: Math.floor(Date.now() / 1000) + 30 * 60, // Current time + 30 minutes
+        mode: "payment",
+        success_url: `${origin}/loading?nextUrl=orders`,
+        cancel_url: `${origin}/cart`,
+        metadata: {
+          orderIds: orderIds.join(","),
+          userId,
+          appId: "vendoor",
+        },
+      });
+      return NextResponse.json({ session });
     }
 
     // Clear the cart
